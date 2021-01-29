5b7b03c3315d843cc5ca816b208cd89dce9f412f
Joshua Williams <joshuawilliamsguitar@gmail.com>
2021-01-28 23:45:59 -0800

    modularize save route

--- src/router/router.js
+++ src/router/router.js
@@ -1,14 +1,15 @@
-const base64 = require('base-64');
 const express = require('express');
 const router = express.Router();
-const { User } = require('../models/userModel');
 
 const signUp = require('./routes/signup');
 const signIn = require('./routes/signin');
 const logout = require('./routes/logout');
+const save = require('./routes/save');
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
+router.post('/save', save);
+
 router.get('/logout', logout);
 
 router.get('/test', (req, res, next) => {
@@ -16,52 +17,5 @@ router.get('/test', (req, res, next) => {
   res.send();
 });
 
-router.post('/save', async (req, res, next) => {
-  try {
-    const songToSave = req.body;
-    const token = req.cookies.token;
-
-    if(!token) { 
-      return next({
-        status: 400,
-        message: 'Must be signed in to save song',
-      });
-    }
-    if(!songToSave) {
-      return next({
-        status: 500,
-        message: 'Missing song to save',
-      });
-    }
-    if(!songToSave.title) {
-      songToSave.title = 'Untitled';
-    }
-
-    const encryptedId = token.split('.')[1];
-    const id = JSON.parse(base64.decode(encryptedId)).id;
-
-    const user = await User.findById(id);
-
-    const currentSongs = user.songs;
-
-    const titles = currentSongs.map(({ title }) => title);
-
-    let i = 1;
-    if(titles.includes(songToSave.title)) {
-      while(titles.includes(songToSave.title + `-${i}`)) {
-        i++;
-      }
-      songToSave.title += `-${i}`;
-    }
-
-    currentSongs.push(songToSave);
-
-    await User.findByIdAndUpdate(id, { songs: currentSongs });
-
-    res.status(201).send(`Song '${songToSave.title}' saved`);
-  } catch (e) {
-    next({ message: e.message });
-  }
-});
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ const router = require('./router/router');
 const generalError = require('./errors/generalError');
 const routeError = require('./errors/404');
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 
 app.use('/api/v1', router);
 app.use('*', routeError);
