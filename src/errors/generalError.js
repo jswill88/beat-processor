c4ae87feb063403ebe5801d6ee50c8eadfa1818c
Joshua Williams <joshuawilliamsguitar@gmail.com>
2021-02-18 19:40:31 -0800

    Add route that logs a user automatically if they have a valid token in their cookie

--- src/errors/generalError.js
+++ src/errors/generalError.js
@@ -2,7 +2,15 @@ module.exports = ({status = 500, message}, _req, res, _next) => {
 
   console.error(message);
 
-  res.status(status).json({ error: message });
+  const error = new Error(message);
+  error.status = status;
+
+  res.status(status).json({
+    error: {
+      message: error.message,
+      status: error.status,
+    },
+  });
 
   res.end();
 
